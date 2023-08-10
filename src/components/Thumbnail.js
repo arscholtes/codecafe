ce27a06a997a3f776b2d678edadd4d9821148e02
arscholtes <alex.scholtes@icloud.com>
2023-08-09 23:37:20 -0500

    proptypes

---
+++ src/components/Thumbnail.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import './Thumbnail.css';
+
+function Thumbnail({ image, title }) {
+  return (
+    <a
+      href="#todo"
+      className="thumbnail-component"
+    >
+      <div>
+        <img src={image} alt={title} />
+      </div>
+    </a>
+  );
+}
+
+Thumbnail.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Thumbnail;
