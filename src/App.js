ca1514a593a9ace3ee99ebd53aa01b8d46dd81c4
arscholtes <alex.scholtes@icloud.com>
2023-12-03 10:14:39 -0500

    added route for Details page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
 import NotFound from './NotFound';
+import Details from './Details';
 import Header from './components/Header';
 import Home from './components/Home';
 
@@ -19,6 +20,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
+        <Route path="/details" element={<Details items={items} /> } />
         <Route path="/" element={<Home items={items} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
