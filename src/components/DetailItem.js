ebfd059069a6f4183147977f37eb6de232d0423c
arscholtes <alex.scholtes@icloud.com>
2023-12-06 04:26:08 -0500

    Not sure why but it is looking like the items should be in the types folder but I do not recal that being mentioned, need to dig back in and fix it

--- src/components/DetailItem.js
+++ src/components/DetailItem.js
@@ -1,13 +1,30 @@
 import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { itemImages } from '../types/item';
+import ItemType from '../types/item';
 import './DetailItem.css';
 
 function DetailItem() {
   const { id } = useParams();
+  const detailItem = items.find((item) => item.itemId === id);
   return (
     <div className="detail-item-component">
-      {id}
+      <img
+        className="details-image"
+        src={itemImages[detailItem.imageId]}
+        alt={detailItem.title}
+      />
+      <h2>{detailItem.price.toFixed(2)}</h2>
+      <div>
+        $
+        {detailItem.price.toFixed(2)}
+      </div>
     </div>
   );
 }
+
+DetailItem.propTypes = {
+  items: PropTypes.arrayOf(ItemType).isRequired,
+};
 
 export default DetailItem;
